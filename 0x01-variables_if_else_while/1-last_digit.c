4b21aa9fd1a494490bcb5bfaceb211de04d25a5d
idmoulay99 <mohamedidmoulay99@gmail.com>
2023-02-17 12:40:36 -0800

    fuck alx

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,9 +14,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d and is ", n, n % 10);
-	if((n % 10) > 5)
+	if ((n % 10) > 5)
 		printf("greater than 5\n");
-	else if((n % 10) == 0)
+	else if ((n % 10) == 0)
 		printf("0\n");
 	else
 		printf("less than 6 and not 0\n");
