a3e2797e9dbf4f43682b9ac58bed3d76ade24ab4
idmoulay99 <mohamedidmoulay99@gmail.com>
2023-02-17 12:38:17 -0800

    follow the fucking c99 standards

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,12 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastDigit;
 	lastDigit = n % 10;
-	printf("Last digit of %d is %d and is ", n, lastDigit);
-	if(lastDigit > 5)
+	printf("Last digit of %d is %d and is ", n, n % 10);
+	if((n % 10) > 5)
 		printf("greater than 5\n");
-	else if(lastDigit == 0)
+	else if((n % 10) == 0)
 		printf("0\n");
 	else
 		printf("less than 6 and not 0\n");
