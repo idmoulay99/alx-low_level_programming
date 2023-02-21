2e2efbf60a8a8df9ad4e909e424495bcdd10be91
idmoulay99 <mohamedidmoulay99@gmail.com>
2023-02-21 08:19:29 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  prints alphabet in lowercase 10 times
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
