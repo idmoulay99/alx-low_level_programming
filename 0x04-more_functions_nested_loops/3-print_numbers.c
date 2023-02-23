7915cd617c9110eee67e606de01822862ff54afd
idmoulay99 <114174957+idmoulay99@users.noreply.github.com>
2023-02-23 16:45:36 +0000

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+/**
+ * print_numbers - checks for uppercase character
+ * followed by new line
+ */
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n')
+}
